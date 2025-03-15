86e4d844768d680aec3ec86c5562527ed978197e
Awaria AD <awaria@wiim.prv>
2025-03-15 10:07:41 +0100

    testy cypress + hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,17 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+const cities = ["Rome", "London", "Paris"];
+const countries = ["Italy", "United Kingdom", "France"];
+describe('Swipe Gallery Test', function () {
+  it('Checks if every\'s slide descprition is correct', function () {
+    cy.visit('http://localhost:3000');
+    for (let i = 0; i < cities.length; i++) {
+      cy.get('.swiper-slide-active').should('contain', cities[i]);
+      cy.get('.swiper-slide-active').should('contain', countries[i]);
+      cy.get('.swiper-button-next').click();
+      cy.wait(2000);  
+    }
+  })
+});
